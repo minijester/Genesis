a50e19a36c70794cddf493f3193df45029721e11
minijester <rt.digital1234@gmail.com>
2016-05-21 19:11:00 +0700

    db function for booksearching

--- src/projectlibrary/database/dbBookSearching.java
+++ src/projectlibrary/database/dbBookSearching.java
@@ -5,6 +5,9 @@
  */
 package projectlibrary.database;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  *
  * @author Minijester
@@ -13,5 +16,26 @@ public class dbBookSearching extends ConnectDB {
     
     public dbBookSearching(){
         super();
+    }
+    
+    public ArrayList<HashMap> searchISBN(String key){
+        String sql = "SELECT * FROM LibraryBook WHERE ISBN = '"+ key + "'";
+        ArrayList<HashMap> list = db.queryRows(sql);
+        boolean delSuccess = db.executeQuery(sql);
+        return list;
+    }
+    
+    public ArrayList<HashMap> searchBookName(String key){
+        String sql2 = "SELECT * FROM LibraryBook WHERE Title = '"+ key + "'";
+        ArrayList<HashMap> list2 = db.queryRows(sql2);
+        boolean delSuccess2 = db.executeQuery(sql2);
+        return list2;
+    }
+    
+    public ArrayList<HashMap> searchAuthor(String key){
+        String sql3 = "SELECT * FROM LibraryBook WHERE Author = '"+ key + "'";
+        ArrayList<HashMap> list3 = db.queryRows(sql3);
+        boolean delSuccess2 = db.executeQuery(sql3);
+        return list3;
     }
 }
